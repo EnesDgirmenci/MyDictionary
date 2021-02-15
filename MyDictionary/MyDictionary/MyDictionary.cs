3afec222c29ca54fc32223deda9cf3ecd2c3a472
İbrahim Enes <49075207+EnesDgirmenci@users.noreply.github.com>
2021-02-16 02:45:37 +0300

    Add files via upload

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] piece;
+        T[] tempPiece;
+
+        //constructor'umuza bu class çalıştığı zaman
+        //bir tane piece isimli 0 elemanlı bir dizi oluşturmasını istedik
+        public MyDictionary()
+        {
+            piece = new T[0];
+        }
+
+        public void Add(T thing)
+        {
+            //piece isimli tabloya yeni bir veri eklenmek istendiği 
+            //zaman öncelikle piece isimli dizide bulunan geçici oluşturduğumuz
+            //tempPiece adlı diziye taşıyoruz
+            //daha sonra piece isimli dizinin uzunluğunu 1 arttırıyoruz
+            tempPiece = piece;
+            piece = new T[piece.Length + 1];
+            //tempPiece isimlidizinin i. elemanını piece dizisinin i. elemanına atıyoruz
+            for (int i = 0; i < tempPiece.Length; i++)
+            {
+                piece[i] = tempPiece[i];
+            }
+            piece[piece.Length - 1] = thing;
+        }
+        public int Count
+        {
+            get { return piece.Length; }
+        }
+    }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> piece1 = new MyDictionary<string>();
+            piece1.Add("Anakart");
+            piece1.Add("Ekran");
+            piece1.Add("İşlemci");
+            piece1.Add("Ekran Kartı");
+            piece1.Add("Ram");
+            piece1.Add("Soğutucu");
+            piece1.Add("Güç Kaynağı");
+            piece1.Add("Klavye");
+            piece1.Add("kasa");
+            Console.WriteLine("Depoda bulunan Pc Parça sayısı: " + piece1.Count);
+        }
+    }
+}
